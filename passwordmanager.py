7a359cca370440ca89b71bab138a103eb8314093
B9nsai <l.stuetz98@gmail.com>
2022-11-23 15:50:07 +0100

    def switch_action, run

--- passwordmanager.py
+++ passwordmanager.py
@@ -2,7 +2,7 @@
 
 class Passwordmanager:
     def __init__(self):
-        pass
+        self.action = 0
 
     def show_menu(self):
         print("=============================================")
@@ -13,11 +13,29 @@ class Passwordmanager:
         print("  3) Abbruch \n")
 
     def read_menu_action(self):
-        return input("Was möchten sie tun? ")
+        return int(input("Was möchten sie tun? "))
+
+    def switch_action(self):
+        match self.action:
+            case 1:
+                self.create_new_database()
+            case 2:
+                self.use_existing_database()
+            case 3:
+                exit()
+
+    def create_new_database(self):
+        pass
+
+    def use_existing_database(self):
+        pass
 
     def run(self):
-        self.show_menu()
-        self.read_menu_action()
+        while True:
+            self.show_menu()
+            self.action = self.read_menu_action()
+            print("")
+            self.switch_action()
 
 passwordmanager = Passwordmanager()
 passwordmanager.run()
