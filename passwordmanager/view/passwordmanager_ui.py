eb5935851c0aa5ff1f0b27c408c46c446e4a9148
B9nsai <l.stuetz98@gmail.com>
2022-11-25 13:33:15 +0100

    adjusted output of ui

--- passwordmanager/view/passwordmanager_ui.py
+++ passwordmanager/view/passwordmanager_ui.py
@@ -3,17 +3,19 @@ sys.path.append('../controller')
 
 from controller import Controller
 
+
+
 def show_startup_menu():
-    print("=============================================")
+    print("\n=============================================")
     print("               Passwordmanager               ")
     print("=============================================")
     print("  1) Neue Passwortdatenbank erstellen        ")
     print("  2) Existierende Passwortdatenbank verwenden")
     print("  3) Abbruch \n")
 
-def show_db_menu():
-        print("=============================================")
-        print("               Passwordmanager " + Controller.db_name)
+def show_db_menu(db_name):
+        print("\n=============================================")
+        print("               Passwordmanager " + db_name)
         print("=============================================")
         print("  1) Bereits existierende Passwörter anzeigen")
         print("  2) Neues Passwort hinzufügen")
