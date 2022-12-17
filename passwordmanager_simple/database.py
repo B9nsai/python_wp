68dce1a884a792badf076dd250a4885c594de3b5
B9nsai <l.stuetz98@gmail.com>
2022-12-17 12:45:58 +0100

    implemented add_new_password and delete_password

--- passwordmanager_simple/database.py
+++ passwordmanager_simple/database.py
@@ -12,5 +12,8 @@ class database:
     def get_list(self):
         return self.__list
 
-    def set_db_list(self, db_list: list):
+    def set_list(self, db_list: list):
         self.__list = db_list
+
+    def add_to_list(self, entry: dict):
+        self.__list.append(entry)
--- passwordmanager_simple/passwordmanager.py
+++ passwordmanager_simple/passwordmanager.py
@@ -42,8 +42,10 @@ class passwordmanager:
         self.run_db_menu()
 
     def run_db_menu(self):
-        self.show_db_menu()
-        self.match_db_menu_option()
+        self.__db_menu_option = 0
+        while(self.__db_menu_option != 5):    
+            self.show_db_menu()
+            self.match_db_menu_option()
 
     def show_db_menu(self):
             print("\n=============================================")
@@ -62,9 +64,9 @@ class passwordmanager:
             case 1:
                 self.show_db()
             case 2:
-                pass
+                self.add_new_password()
             case 3:
-                pass
+                self.delete_password()
             case 4:
                 pass
             case 5:
@@ -76,6 +78,39 @@ class passwordmanager:
         print("=============================================")
         for line in self.__db.get_list():
             print(line)
+
+    def add_new_password(self):
+        url = str(input("URL: "))
+        name = str(input("Name: "))
+        password = str(input("Passwort: "))
+        note = str(input("Notiz: "))
+
+        entry = {
+            "url": url,
+            "name": name,
+            "password": password,
+            "note": note
+        }
+
+        self.__db.add_to_list(entry)
+
+    def delete_password(self):
+        pass
+        url = str(input("URL des zu löschenden Passworts: "))
+
+        list_copy = self.__db.get_list().copy()
+
+        is_deleted = False
+
+        for line in list_copy:
+            if line.get("url") == url:
+                self.__db.get_list().remove(line)
+                is_deleted = True
+
+        if is_deleted:
+            print("Löschen erfolgreich")
+        else:
+            print("URL nicht gefunden")
 
 pw_manager = passwordmanager()
 pw_manager.main()
